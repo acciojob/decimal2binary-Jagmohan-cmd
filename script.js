57239f6a322609ccb0985d5290fb31b46ac196f8
Jagmohan-cmd <141887538+Jagmohan-cmd@users.noreply.github.com>
2023-10-18 16:48:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 function decimalToBinary(num) {
   //Write you code here
+	let output = "";
+	while(num >= 2){
+		let reminder = num % 2;
+		output = reminder + output;
+		num = parseInt(num / 2);
+	}
+
+	if(num === 1){
+		output = 1 + output;
+	}
+	return output;
   
 }
 
